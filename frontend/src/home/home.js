5a3e06eba817e0253e8d50b01304012d565f411b
busraceper <bsceper@gmail.com>
2024-04-07 15:27:24 +0200

    background fix

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -6,38 +6,41 @@ import backgroundImage from './homebackground2.jpg'
 import {useNavigate} from "react-router-dom";
 
 
-    const Home = () => {
-        const [name, setName] = useState('');
-        const navigate = useNavigate();
+const Home = () => {
+    const [name, setName] = useState('');
+    const navigate = useNavigate();
 
-        const handleInputChange = (e) => {
-            setName(e.target.value);
-        };
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        const regex = /^[A-Za-z]+$/;
 
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-            try {
-                const response = await fetch('http://localhost:8080/player/save', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ username: name }),
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log(data);
-                    localStorage.setItem('playerId', data.id);
-                    navigate("/game");
-                } else {
-                    console.error('Failed to send name to the API');
-                }
-            } catch (error) {
-                console.error('Error submitting name:', error);
+        if (value === '' || regex.test(value)) {
+            setName(value);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/player/save', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({username: name}),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                localStorage.setItem('playerId', data.id);
+                navigate("/game");
+            } else {
+                console.error('Failed to send name to the API');
             }
-        };
-
-
+        } catch (error) {
+            console.error('Error submitting name:', error);
+        }
+    };
 
     return (
         <div className="container" style={{
@@ -73,6 +76,6 @@ import {useNavigate} from "react-router-dom";
             </div>
         </div>
     );
-    };
+};
 
 export default Home;
